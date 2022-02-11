3f4de5a1e7d3e354ddea5dc9c9ad2452cb8b5f20
Okunator <oskari.lehtonen@helsinki.fi>
2022-02-11 16:09:34 +0200

    tests(transforms): add rest of the tests

---
+++ cellseg_models_pytorch/transforms/tests/test_inst_transforms.py
@@ -0,0 +1,65 @@
+import numpy as np
+import pytest
+
+from cellseg_models_pytorch.transforms import (
+    apply_each,
+    cellpose_transform,
+    compose,
+    contour_transform,
+    dist_transform,
+    edgeweight_transform,
+    hovernet_transform,
+    omnipose_transform,
+    smooth_dist_transform,
+)
+from cellseg_models_pytorch.utils.tests.fixtures import inst_map
+
+
+@pytest.mark.parametrize(
+    "transform",
+    [cellpose_transform, hovernet_transform, omnipose_transform],
+)
+@pytest.mark.parametrize("zero_input", [None, np.zeros((14, 14))])
+def test_gradient_transforms(inst_map, transform, zero_input):
+    trans = compose([transform()])
+
+    if zero_input is not None:
+        inst_map = zero_input
+
+    transformed = trans(image=inst_map, inst_map=inst_map)
+
+    assert transformed["inst_map"].dtype == np.float64
+    assert transformed["inst_map"].shape == (2,) + inst_map.shape
+
+
+@pytest.mark.parametrize(
+    "transform",
+    [dist_transform, smooth_dist_transform, contour_transform, edgeweight_transform],
+)
+@pytest.mark.parametrize("zero_input", [None, np.zeros((14, 14))])
+def test_inst_transforms(inst_map, transform, zero_input):
+    trans = compose([transform()])
+
+    if zero_input is not None:
+        inst_map = zero_input
+
+    transformed = trans(image=inst_map, inst_map=inst_map)
+
+    assert transformed["inst_map"].dtype == np.float64
+    assert transformed["inst_map"].shape == inst_map.shape
+
+
+@pytest.mark.parametrize("zero_input", [None, np.zeros((14, 14))])
+def test_inst_transform_pipeline(inst_map, zero_input):
+    pipeline = apply_each(
+        [edgeweight_transform(), cellpose_transform(), smooth_dist_transform()]
+    )
+
+    if zero_input is not None:
+        inst_map = zero_input
+
+    transformed = pipeline(image=inst_map, inst_map=inst_map)
+
+    assert transformed["edgeweight"]["inst_map"].dtype == np.float64
+    assert transformed["cellpose"]["inst_map"].dtype == np.float64
+    assert transformed["smoothdist"]["inst_map"].dtype == np.float64
