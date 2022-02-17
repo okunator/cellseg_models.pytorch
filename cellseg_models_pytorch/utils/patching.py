845b2052953450968c7e5fc81bf043ae1050bc7b
Okunator <oskari.lehtonen@helsinki.fi>
2022-02-17 15:25:33 +0200

    feat(patching): add tensor patching utilities

--- cellseg_models_pytorch/utils/patching.py
+++ cellseg_models_pytorch/utils/patching.py
@@ -1,9 +1,16 @@
 from typing import Tuple
 
 import numpy as np
+import torch
+import torch.nn.functional as F
 from skimage.util import view_as_windows
 
-__all__ = ["_get_margins", "extract_patches", "stitch_patches", "TilerStitcher"]
+__all__ = [
+    "_get_margins",
+    "extract_patches_numpy",
+    "stitch_patches_numpy",
+    "TilerStitcher",
+]
 
 
 def _get_margins(
@@ -21,7 +28,7 @@ def _get_margins(
             Height of one image patch.
         patch_width : int
             Width of one image patch.
-        pad : int
+        pad : int, default=200
             Padding in pixels.
 
     Returns
@@ -35,7 +42,7 @@ def _get_margins(
     return int(y), int(x)
 
 
-def extract_patches(
+def extract_patches_numpy(
     im: np.ndarray, stride: int, patch_shape: Tuple[int, int, int], padding: bool = True
 ) -> Tuple[np.ndarray, int, int]:
     """Extract overlapping patches from an image.
@@ -48,7 +55,7 @@ def extract_patches(
             Stride for the sliding window (pixels).
         patch_shape : Tuple[int, int, int]
             Height, width and num of channels of the patches.
-        padding : bool
+        padding : bool, default=True
             Use reflection padding to make the input shape divisible
             by the stride size.
 
@@ -97,7 +104,7 @@ def extract_patches(
     )
 
 
-def stitch_patches(
+def stitch_patches_numpy(
     patches: np.ndarray,
     orig_shape: Tuple[int, ...],
     ny: int,
@@ -113,8 +120,8 @@ def stitch_patches(
     ----------
         patches : np.ndarray
             Patched image. Shape (N, pH, pW, C).
-        orig_shape Tuple[int, int, int]:
-            Shape of the original image.
+        orig_shape : Tuple[int, int, int]
+            Shape of the original image. Format (H, W, C).
         ny : int
             Number of rows in patch grid.
         nx : int
@@ -140,9 +147,103 @@ def stitch_patches(
     patches = patches[:, :, 0:pad, 0:pad, :]
     stitched = np.concatenate(np.concatenate(patches, 1), 1)
 
-    stitched = stitched[pad_x : orig_shape[0] + pad_x, pad_y : orig_shape[1] + pad_y, :]
+    stitched = stitched[pad_y : orig_shape[0] + pad_y, pad_x : orig_shape[1] + pad_x, :]
 
     return stitched
+
+
+def extract_patches_torch(
+    batch: torch.Tensor, stride: int, patch_shape: Tuple[int, int], padding: bool = True
+) -> torch.Tensor:
+    """Extract patches from a batched tensor.
+
+    Parameters
+    ----------
+        batch : torch.Tensor
+            Batched input images. Shape (B, C, H, W).
+        stride : int
+            Stride for the sliding window (pixels).
+        patch_shape : Tuple[int, int]
+            Height, width and num of channels of the patches.
+        padding : bool, default=True
+            Use reflection padding to make the input shape divisible
+            by the stride size.
+
+    Returns
+    -------
+        torch.Tensor:
+            patched batch. Shape (B, C, n_patches, patch_height, patch_width).
+
+    Raises
+    ------
+        ValueError: If input has wrong shape.
+    """
+    if batch.dim() != 4:
+        raise ValueError(
+            f"`batch` shape needs to be in format (B, C, H, W). Got: {batch.shape}."
+        )
+
+    if padding:
+        pad_y, pad_x = _get_margins(
+            batch.shape[-2], batch.shape[-1], patch_shape[0], patch_shape[1]
+        )
+        batch = F.pad(batch.float(), [pad_x, pad_x, pad_y, pad_y], mode="reflect")
+
+    dims = list(range(2, batch.dim()))
+    for dim, patch_size in zip(dims, patch_shape):
+        batch = batch.unfold(dim, patch_size, stride)
+
+    patches = batch.contiguous().view(
+        batch.shape[0], batch.shape[1], -1, patch_shape[0], patch_shape[1]
+    )
+
+    return patches
+
+
+def stitch_patches_torch(
+    patches: torch.Tensor, orig_shape: Tuple[int, int, int], stride: int, padding: bool
+) -> torch.Tensor:
+    """Stitch tensor patches back to one batched image.
+
+    Parameters
+    ----------
+        patches : torch.Tensor
+            Input tensor patches. Shape (B, C, n_patches, H, W).
+        orig_shape : Tuple[int, int, int]
+            Shape of the original image. Format (H, W, C).
+        stride : int
+            Stride of the sliding window that was used for patching.
+        padding : bool
+            Flag, whether padding was used during patching.
+
+    Returns
+    -------
+        torch.Tensor:
+            Original sized batch. Shape: (B, C, H, W).
+    """
+    B, C, _, ph, pw = patches.shape
+    H, W = orig_shape[2:]
+
+    pad_y = 0
+    pad_x = 0
+    if padding:
+        pad_y, pad_x = _get_margins(H, W, ph, pw)
+        H += pad_y * 2
+        W += pad_x * 2
+
+    patches = patches.float().contiguous().view(B, C, -1, ph * pw)
+    patches = patches.permute(0, 1, 3, 2)
+
+    patches = patches.contiguous().view(B, C * ph * pw, -1)
+    output = F.fold(patches, output_size=(H, W), kernel_size=ph, stride=stride)
+
+    recovery_mask = F.fold(
+        torch.ones_like(patches), output_size=(H, W), kernel_size=ph, stride=stride
+    )
+    output = output / recovery_mask
+    output = output[:, :, pad_y : H - pad_y, pad_x : W - pad_x]
+
+    return output
 
 
 class TilerStitcher:
@@ -154,7 +255,7 @@ class TilerStitcher:
         padding: bool = True,
     ) -> None:
         """
-        Image patcher-stitcher.
+        Numpy image patcher-stitcher.
 
         Parameters
         ----------
@@ -165,27 +266,27 @@ class TilerStitcher:
             stride : int
                 Stride size for the sliding window.
             padding : bool, default=True
-                Use reflection padding (To get a square image.). If set to False,
-                the `backstitch` is unlikely able to recover the full sized original
-                image but instead cropped image.
+                Use reflection padding. If set to False, the `backstitch`
+                is unlikely able to recover the full sized original image
+                but instead cropped image.
 
         Example
         -------
-        >>> im = read_img("/<path>/<to>/img.png)
-        >>> print(im.shape)
-        (1153, 1307, 3)
-        >>> tiler = TilerStitcher(
-                im_shape=im.shape,
-                patch_shape=(256, 256, 3),
-                stride=80,
-                padding=True
-            )
-        >>> patches = tiler.patch(im)
-        >>> print(patches.shape)
-        (323, 256, 256, 3)
-        >>> orig = tiler.backstitch(patches)
-        >>> print(orig.shape)
-        (1153, 1307, 3)
+            >>> im = read_img("/<path>/<to>/img.png)
+            >>> print(im.shape)
+            (1153, 1307, 3)
+            >>> tiler = TilerStitcher(
+                    im_shape=im.shape,
+                    patch_shape=(256, 256, 3),
+                    stride=80,
+                    padding=True
+                )
+            >>> patches = tiler.patch(im)
+            >>> print(patches.shape)
+            (323, 256, 256, 3)
+            >>> orig = tiler.backstitch(patches)
+            >>> print(orig.shape)
+            (1153, 1307, 3)
         """
         self.im_shape = im_shape
         self.patch_shape = patch_shape
@@ -201,12 +302,13 @@ class TilerStitcher:
         ----------
             im : np.ndarray
                 Input image. Shape: (H, W)|(H, W, C).
+
         Returns
         -------
             np.ndarray:
                 Patched input image. Shape: (N, pH, pW, C).
         """
-        patches, self.ny, self.nx = extract_patches(
+        patches, self.ny, self.nx = extract_patches_numpy(
             im, self.stride, self.patch_shape, self.padding
         )
 
@@ -225,7 +327,87 @@ class TilerStitcher:
             np.ndarray:
                 Backstitched input image. Shape: (H, W, C).
         """
-        stitched = stitch_patches(
+        stitched = stitch_patches_numpy(
             patches, self.im_shape, self.ny, self.nx, self.stride, self.padding
         )
         return stitched.squeeze()
+
+
+class TilerStitcherTorch:
+    def __init__(
+        self,
+        batch_shape: Tuple[int, int, int, int],
+        patch_shape: Tuple[int, int],
+        stride: int,
+        padding: bool = True,
+    ) -> None:
+        """Patch extractor and stitcher class.
+
+        Operates on batched torch tensors.
+        I.e tensors of shape: (B, C, H, W).
+
+        Parameters
+        ----------
+            batch_shape : Tuple[int, int, int, int]
+                Input batch image shape. Has to be in format: (B, C, H, W).
+            patch_shape : Tuple[int, int]
+                Shape of one patch (pH, pW).
+            stride : int
+                Stride size for the sliding window.
+            padding : bool, default=True
+                Use reflection padding (To get a square image.). If set to False,
+                the `backstitch` is unlikely able to recover the full sized original
+                image but instead cropped image.
+
+        Example
+        -------
+            >>> t = torch.rand([2, 1, 1100, 1200])
+            >>> ts = TilerStitcherTorch((2, 1, 1100, 1200), (256, 256), 80, padding=1)
+            >>> patches = ts.patch(t)
+            >>> print(patches.shape)
+            torch.Size([2, 1, 272, 256, 256])
+
+            >>> stitched = ts.backstitch(patches)
+            >>> print(stitched.shape)
+            torch.Size([2, 1, 1100, 1200])
+        """
+        self.batch_shape = batch_shape
+        self.patch_shape = patch_shape
+        self.stride = stride
+        self.padding = padding
+
+    def patch(self, im: torch.Tensor) -> torch.Tensor:
+        """Extract patches from an input image.
+
+        Parameters
+        ----------
+            im : torch.Tensor
+                Input image. Shape: (B, C, H, W).
+
+        Returns
+        -------
+            torch.Tensor:
+                Patched input. Shape: (B, C, n_patches, patch_height, patch_width).
+        """
+        patches = extract_patches_torch(im, self.stride, self.patch_shape, self.padding)
+
+        return patches
+
+    def backstitch(self, patches: torch.Tensor) -> torch.Tensor:
+        """Stitch patches back to the original image size.
+
+        Parameters
+        ----------
+            patches : torch.Tensor
+                Patched image. Shape: (B, C, n_patches, patch_height, patch_width).
+
+        Returns
+        -------
+            np.ndarray:
+                Backstitched input image. Shape: (B, C, H, W).
+        """
+        stitched = stitch_patches_torch(
+            patches, self.batch_shape, self.stride, self.padding
+        )
+
+        return stitched
