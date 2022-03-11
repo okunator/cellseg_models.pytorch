a0af943f06f36619eaec9edab428e4d47e730b1c
Okunator <oskari.lehtonen@helsinki.fi>
2022-03-11 16:17:11 +0200

    feat(losses): add joint loss an multi-task-loss

---
+++ cellseg_models_pytorch/losses/joint_loss.py
@@ -0,0 +1,68 @@
+from typing import List
+
+import torch
+import torch.nn as nn
+
+
+class JointLoss(nn.ModuleDict):
+    def __init__(
+        self,
+        losses: List[nn.Module],
+        weights: List[float] = None,
+    ) -> None:
+        """Joint loss function.
+
+        Takes in a list of nn.Module losses and computes the loss for
+        each loss in the list and at the end sums the outputs together
+        as one joint loss.
+
+        Parameters
+        ----------
+            losses : List[nn.Module]
+                List of initialized nn.Module losses.
+            weights : List[float], default=None
+                List of weights for each loss.
+
+        Raises
+        ------
+            ValueError:
+                If more than 4 losses are given as input.
+                If given weights are not between [0, 1].
+        """
+        super().__init__()
+
+        if not isinstance(losses, list):
+            raise ValueError("`losses` arg must be a list.")
+
+        if len(losses) > 4:
+            raise ValueError(
+                f"""
+                Currently the max number of losses in one JointLoss is 4.
+                Got: {len(losses)}. {losses}"""
+            )
+
+        self.weights = [1.0] * len(losses)
+        if weights is not None:
+            if not all(0 <= val <= 1.0 for val in weights):
+                raise ValueError(f"Weights need to be 0 <= weight <= 1. Got: {weights}")
+            self.weights = weights
+
+        for i in range(len(losses)):
+            self.add_module(f"loss{i + 1}", losses[i])
+
+    def forward(self, **kwargs) -> torch.Tensor:
+        """Compute the joint-loss.
+
+        Returns
+        -------
+            torch.Tensor:
+                The computed joint loss.
+        """
+        lw = zip(self.values(), self.weights)
+        losses = torch.stack([loss(**kwargs) * weight for loss, weight in lw])
+        return torch.sum(losses)
+
+    def extra_repr(self) -> str:
+        """Add info to print."""
+        s = "loss_weights={weights}"
+        return s.format(**self.__dict__)
---
+++ cellseg_models_pytorch/losses/multitask_loss.py
@@ -0,0 +1,91 @@
+from typing import Dict
+
+import torch
+import torch.nn as nn
+
+from .joint_loss import JointLoss
+
+
+class MultiTaskLoss(nn.ModuleDict):
+    def __init__(
+        self,
+        branch_losses: Dict[str, JointLoss],
+        loss_weights: Dict[str, float] = None,
+        **kwargs,
+    ) -> None:
+        """Multi-task loss wrapper.
+
+        Combines losses from different branches to one loss function.
+
+        Parameters
+        ----------
+            branch_losses : Dict[str, nn.Module]
+                Dictionary of branch names mapped to a loss module.
+                e.g. {"inst": JointLoss(MSE(), Dice()), "type": Dice()}.
+            loss_weights : Dict[str, float], default=None
+                Dictionary of branch names mapped to the weight used for
+                that branch loss.
+
+        Raises
+        ------
+            ValueError:
+                If the input arguments have different lengths.
+                If the input arguments have mismatching keys.
+        """
+        super().__init__()
+
+        self.weights = {k: 1.0 for k in branch_losses.keys()}
+        if loss_weights is not None:
+            if len(loss_weights) != len(branch_losses):
+                raise ValueError(
+                    f"""
+                    Got {len(loss_weights)} loss weights and {len(branch_losses)}
+                    branches. Need to have the same length."""
+                )
+            if not all(k in branch_losses.keys() for k in loss_weights.keys()):
+                raise ValueError(
+                    """Mismatching keys in `loss_weights` and `branch_losses`"""
+                )
+            else:
+                self.weights = loss_weights
+
+        for branch, loss in branch_losses.items():
+            self.add_module(f"{branch}_loss", loss)
+
+    def forward(
+        self, yhats: Dict[str, torch.Tensor], targets: Dict[str, torch.Tensor], **kwargs
+    ) -> torch.Tensor:
+        """Compute the joint loss of the multi-task network.
+
+        Parameters
+        ----------
+            yhats : Dict,[str, torch.Tensor]
+                Dictionary of branch names mapped to the predicted masks.
+            targets : Dict,[str, torch.Tensor]
+                Dictionary of branch names mapped to the GT masks.
+
+        Returns
+        -------
+            torch.Tensor:
+                Computed multi-task loss (Scalar).
+        """
+        weight_map = None
+        if "weight_map" in targets.keys():
+            weight_map = targets["weight_map"]
+
+        multitask_loss = 0.0
+        for branch, loss in self.items():
+            branch = branch.split("_")[0]
+            branch_loss = loss(
+                yhat=yhats[f"{branch}_map"],
+                target=targets[f"{branch}_map"],
+                target_weight=weight_map,
+            )
+            multitask_loss += branch_loss * self.weights[branch]
+
+        return multitask_loss
+
+    def extra_repr(self) -> str:
+        """Add info to print."""
+        s = "branch_loss_weights={weights}"
+        return s.format(**self.__dict__)
