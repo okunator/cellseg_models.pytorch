0cba7deb4f310b8340b1e799f38c0c7da7d281d1
Okunator <oskari.lehtonen@helsinki.fi>
2022-03-11 16:17:36 +0200

    tests(losses): add tests

---
+++ cellseg_models_pytorch/losses/tests/test_multitask_loss.py
@@ -0,0 +1,35 @@
+from collections import OrderedDict
+
+import pytest
+import torch
+
+from cellseg_models_pytorch.losses import (
+    SSIM,
+    CELoss,
+    IoULoss,
+    JointLoss,
+    MultiTaskLoss,
+    TverskyLoss,
+)
+from cellseg_models_pytorch.losses.tests.test_losses import _get_dummy_pair
+
+
+@torch.no_grad()
+@pytest.mark.parametrize("n_classes", [1, 3])
+@pytest.mark.parametrize(
+    "losses",
+    [
+        {"inst": JointLoss([TverskyLoss(), IoULoss()])},
+        {"inst": CELoss(), "type": SSIM()},
+    ],
+)
+def test_multitask_loss(n_classes, losses):
+    losses = OrderedDict(losses)
+
+    yhats = {}
+    targets = {}
+    for i, br in enumerate(losses.keys(), 1):
+        yhats[f"{br}_map"], targets[f"{br}_map"] = _get_dummy_pair(n_classes + i)
+
+    mtl = MultiTaskLoss(losses)
+    mtl(yhats, targets)
