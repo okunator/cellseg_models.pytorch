e7f7b264a030f0f380b549bc0f6611898f15b5cb
Okunator <oskari.lehtonen@helsinki.fi>
2022-06-08 13:46:20 +0300

    feat(optim): Add optimizer utilities.

--- cellseg_models_pytorch/optimizers/utils.py
+++ cellseg_models_pytorch/optimizers/utils.py
@@ -4,17 +4,21 @@ import torch.nn as nn
 
 
 def adjust_optim_params(
-    model: nn.Module, encoder_lr: float, encoder_wd: float, remove_bias_wd: bool = True
+    model: nn.Module,
+    encoder_lr: float,
+    encoder_wd: float,
+    decoder_lr: float,
+    decoder_wd: float,
+    remove_bias_wd: bool = True,
 ) -> List[Dict[str, Dict]]:
-    """Adjust the model parameters for optimizer.
+    """Adjust the learning parameters for optimizer.
 
     1. Adjust learning rate and weight decay in the pre-trained
-        encoder. Lower lr in encoder assumes that the encoder is
-        already close to an optimum.
-    2. Remove weight decay from bias terms to reduce overfitting
+        encoder and decoders.
+    2. Remove weight decay from bias terms to reduce overfitting.
 
     "Bag of Tricks for Image Classification with Convolutional Neural Networks"
-    https://arxiv.org/pdf/1812.01187
+    - https://arxiv.org/pdf/1812.01187
 
     Parameters
     ----------
@@ -24,8 +28,13 @@ def adjust_optim_params(
             Learning rate of the model encoder.
         encoder_wd : float
             Weight decay for the model encoder.
+        decoder_lr : float
+            Learning rate of the model decoder.
+        decoder_wd : float
+            Weight decay for the model decoder.
         remove_bias_wd : bool, default=True
-            Flag to whether to remove the weight decay from the bias terms.
+            If True, the weight decay from the bias terms is removed from the model
+            params. Ignored if `remove_wd`=True.
 
     Returns
     -------
@@ -33,15 +42,21 @@ def adjust_optim_params(
             a list of kwargs (str, Dict pairs) containing the model params.
     """
     params = list(model.named_parameters())
-    encoder_params = {"encoder": dict(lr=encoder_lr, weight_decay=encoder_wd)}
+    encoder_params = {"encoder": {"lr": encoder_lr, "weight_decay": encoder_wd}}
+    decoder_params = {"decoder": {"lr": decoder_lr, "weight_decay": decoder_wd}}
 
     adjust_params = []
     for name, parameters in params:
         opts = {}
-        for key, key_opts in encoder_params.items():
-            if key in name:
-                for k, i in key_opts.items():
-                    opts[k] = i
+        for enc, enc_opts in encoder_params.items():
+            if enc in name:
+                for key, item in enc_opts.items():
+                    opts[key] = item
+
+        for dec, dec_opts in decoder_params.items():
+            if dec in name:
+                for key, item in dec_opts.items():
+                    opts[key] = item
 
         if remove_bias_wd:
             if name.endswith("bias"):
