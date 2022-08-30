32e3367398d91e84db3087495237e80b6f3168e1
Okunator <oskari.lehtonen@helsinki.fi>
2022-08-30 15:08:39 +0300

    chore: update version

--- cellseg_models_pytorch/__init__.py
+++ cellseg_models_pytorch/__init__.py
@@ -1,7 +1,7 @@
 from . import datamodules, datasets, inference, models, training, utils
 from .models import CellPoseUnet, HoverNet, StarDistUnet
 
-__version__ = "0.1.0.dev0"
+__version__ = "0.1.0"
 submodules = ["utils", "models", "inference", "training", "datasets", "datamodules"]
 __all__ = [
     "__version__",
