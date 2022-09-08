8eca06c8e2faeb14a59166283e705b76f7617596
Okunator <oskari.lehtonen@helsinki.fi>
2022-09-08 14:23:28 +0300

    chore: add scriv as dev dep, bump version

--- cellseg_models_pytorch/__init__.py
+++ cellseg_models_pytorch/__init__.py
@@ -1,7 +1,7 @@
 from . import inference, models, utils
 from .models import CellPoseUnet, HoverNet, StarDistUnet
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 submodules = ["utils", "models", "inference"]
 __all__ = [
     "__version__",
