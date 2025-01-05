a7ea0ec1c817c7e541040ead07be49845eceafb5
Oskari <oskari.lehtonen@helsinki.fi>
2025-01-05 20:35:54 +0200

    fix: fix dataset import in inference

--- cellseg_models_pytorch/inference/__init__.py
+++ cellseg_models_pytorch/inference/__init__.py
@@ -1,5 +1,5 @@
+from ..torch_datasets.folder_dataset_infer import FolderDatasetInfer
 from ._base_inferer import BaseInferer
-from .folder_dataset_infer import FolderDatasetInfer
 from .post_processor import PostProcessor
 from .predictor import Predictor
 from .resize_inferer import ResizeInferer
