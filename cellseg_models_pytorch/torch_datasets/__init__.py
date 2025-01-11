41e01055a0d84b10b9a519bc73b69d6cba1403aa
Oskari <oskari.lehtonen@helsinki.fi>
2025-01-11 16:53:19 +0200

    feat: add new `torch_datasets` module

--- cellseg_models_pytorch/torch_datasets/__init__.py
+++
@@ -1,4 +0,0 @@
-from .folder_dataset_train import SegmentationFolderDataset
-from .hdf5_dataset_train import SegmentationHDF5Dataset
-
-__all__ = ["FolderDataset", "SegmentationFolderDataset", "SegmentationHDF5Dataset"]
