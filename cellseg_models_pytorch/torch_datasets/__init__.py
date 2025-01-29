b701012f84983dea4e3ea5b8fb08e2825a001580
Oskari <oskari.lehtonen@helsinki.fi>
2025-01-29 10:32:43 +0200

    fix: add imports to torch_dataset __init__.py

--- cellseg_models_pytorch/torch_datasets/__init__.py
+++ cellseg_models_pytorch/torch_datasets/__init__.py
@@ -1,5 +1,11 @@
 from .folder_dataset_infer import FolderDatasetInfer
+from .folder_dataset_train import TrainDatasetFolder
 from .hdf5_dataset_train import TrainDatasetH5
 from .wsi_dataset_infer import WSIDatasetInfer
 
-__all__ = ["FolderDatasetInfer", "WSIDatasetInfer", "TrainDatasetH5"]
+__all__ = [
+    "FolderDatasetInfer",
+    "WSIDatasetInfer",
+    "TrainDatasetH5",
+    "TrainDatasetFolder",
+]
