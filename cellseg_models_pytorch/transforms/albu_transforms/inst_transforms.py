7036689c9c87263be8054b41e242f0ca5de08bac
Oskari <oskari.lehtonen@helsinki.fi>
2025-02-12 15:58:41 +0200

    feat: make the de-duplication of instance labels optional in all of the instance transforms to avoid overhead when not needed

--- cellseg_models_pytorch/transforms/albu_transforms/inst_transforms.py
+++ cellseg_models_pytorch/transforms/albu_transforms/inst_transforms.py
@@ -29,14 +29,20 @@ __all__ = [
 
 
 class CellposeTransform(OnlyInstMapTransform):
-    def __init__(self) -> None:
+    def __init__(self, deduplicate: bool = True) -> None:
         """Generate flows from a heat diffused label mask.
 
         https://www.nature.com/articles/s41592-020-01018-x
+
+        Parameters:
+            deduplicate (bool, default=True):
+                Whether to fix duplicate values in the mask before transforming.
+                This adds overhead, so use only if you know that the mask has duplicates.
         """
         super().__init__()
         self.name = "cellpose"
         self.out_channels = 2
+        self.deduplicate = deduplicate
 
     def __call__(self, inst: np.ndarray, **kwargs) -> np.ndarray:
         """Fix duplicate values and generate flows.
@@ -50,18 +56,25 @@ class CellposeTransform(OnlyInstMapTransform):
                 Horizontal and vertical flows of objects.
                 Shape: (2, H, W). Dtype: float64.
         """
-        return gen_flow_maps(fix_duplicates(inst))
+
+        return gen_flow_maps(inst)
 
 
 class HoVerNetTransform(OnlyInstMapTransform):
-    def __init__(self) -> None:
+    def __init__(self, deduplicate: bool = True) -> None:
         """Generate horizontal and vertical gradients from a label mask.
 
         https://www.sciencedirect.com/science/article/pii/S1361841519301045
+
+        Parameters:
+            deduplicate (bool, default=True):
+                Whether to fix duplicate values in the mask before transforming.
+                This adds overhead, so use only if you know that the mask has duplicates.
         """
         super().__init__()
         self.name = "hovernet"
         self.out_channels = 2
+        self.deduplicate = deduplicate
 
     def __call__(self, inst: np.ndarray, **kwargs) -> np.ndarray:
         """Fix duplicate values and generate gradients.
@@ -75,18 +88,26 @@ class HoVerNetTransform(OnlyInstMapTransform):
                 Horizontal and vertical gradients of objects.
                 Shape: (2, H, W). Dtype: float64.
         """
-        return gen_hv_maps(fix_duplicates(inst))
+        if self.deduplicate:
+            inst = fix_duplicates(inst)
+        return gen_hv_maps(inst)
 
 
 class OmniposeTransform(OnlyInstMapTransform):
-    def __init__(self):
+    def __init__(self, deduplicate: bool = True) -> None:
         """Generate horizontal and vertical eikonal flows from a label mask.
 
         https://www.biorxiv.org/content/10.1101/2021.11.03.467199v2
+
+        Parameters:
+            deduplicate (bool, default=True):
+                Whether to fix duplicate values in the mask before transforming.
+                This adds overhead, so use only if you know that the mask has duplicates.
         """
         super().__init__()
         self.name = "omnipose"
         self.out_channels = 2
+        self.deduplicate = deduplicate
 
     def __call__(self, inst: np.ndarray, **kwargs) -> np.ndarray:
         """Fix duplicate values and generate eikonal flows.
@@ -100,23 +121,29 @@ class OmniposeTransform(OnlyInstMapTransform):
                 Horizontal and vertical gradients of objects.
                 Shape: (2, H, W). Dtype: float64.
         """
-        return gen_omni_flow_maps(fix_duplicates(inst))
+        if self.deduplicate:
+            inst = fix_duplicates(inst)
+        return gen_omni_flow_maps(inst)
 
 
 class StardistTransform(OnlyInstMapTransform):
-    def __init__(self, n_rays: int = 32, **kwargs):
+    def __init__(self, n_rays: int = 32, deduplicate: bool = True, **kwargs):
         """Generate radial distance maps from a label mask.
 
         https://arxiv.org/abs/1806.03535
 
-        Parameters
+        Parameters:
             n_rays (int, default=32):
                 Number of rays used for computing distance maps.
+            deduplicate (bool, default=True):
+                Whether to fix duplicate values in the mask before transforming.
+                This adds overhead, so use only if you know that the mask has duplicates.
         """
         super().__init__()
         self.name = "stardist"
         self.n_rays = n_rays
         self.out_channels = n_rays
+        self.deduplicate = deduplicate
 
     def __call__(self, inst: np.ndarray, **kwargs) -> np.ndarray:
         """Fix duplicate values and generate radial distance maps.
@@ -130,18 +157,26 @@ class StardistTransform(OnlyInstMapTransform):
                 Pixelwise radial distance maps.
                 Shape: (n_rays, H, W). Dtype: float64.
         """
-        return gen_stardist_maps(fix_duplicates(inst), self.n_rays)
+        if self.deduplicate:
+            inst = fix_duplicates(inst)
+        return gen_stardist_maps(inst, self.n_rays)
 
 
 class SmoothDistTransform(OnlyInstMapTransform):
-    def __init__(self):
+    def __init__(self, deduplicate: bool = True):
         """Generate FIM distance transforms from a label mask.
 
         https://www.biorxiv.org/content/10.1101/2021.11.03.467199v2
+
+        Parameters:
+            deduplicate (bool, default=True):
+                Whether to fix duplicate values in the mask before transforming.
+                This adds overhead, so use only if you know that the mask has duplicates.
         """
         super().__init__()
         self.name = "smoothdist"
         self.out_channels = 1
+        self.deduplicate = deduplicate
 
     def __call__(self, inst: np.ndarray, **kwargs) -> np.ndarray:
         """Generate smooth distance transforms.
@@ -155,15 +190,24 @@ class SmoothDistTransform(OnlyInstMapTransform):
                 Smooth distance transforms of objects.
                 Shape: (H, W). Dtype: float64.
         """
+        if self.deduplicate:
+            inst = fix_duplicates(inst)
         return smooth_distance(inst)
 
 
 class DistTransform(OnlyInstMapTransform):
-    def __init__(self) -> None:
-        """Generate distance transforms from a label mask."""
+    def __init__(self, deduplicate: bool = True) -> None:
+        """Generate distance transforms from a label mask.
+
+        Parameters:
+            deduplicate (bool, default=True):
+                Whether to fix duplicate values in the mask before transforming.
+                This adds overhead, so use only if you know that the mask has duplicates.
+        """
         super().__init__()
         self.name = "dist"
         self.out_channels = 1
+        self.deduplicate = deduplicate
 
     def __call__(self, inst: np.ndarray, **kwargs) -> np.ndarray:
         """Generate distance transforms.
@@ -177,15 +221,24 @@ class DistTransform(OnlyInstMapTransform):
                 Distance transforms of objects.
                 Shape: (H, W). Dtype: float64.
         """
-        return gen_dist_maps(fix_duplicates(inst))
+        if self.deduplicate:
+            inst = fix_duplicates(inst)
+        return gen_dist_maps(inst)
 
 
 class ContourTransform(OnlyInstMapTransform):
-    def __init__(self):
-        """Generate contour map from a label mask."""
+    def __init__(self, deduplicate: bool = True):
+        """Generate contour map from a label mask.
+
+        Parameters:
+            deduplicate (bool, default=True):
+                Whether to fix duplicate values in the mask before transforming.
+                This adds overhead, so use only if you know that the mask has duplicates.
+        """
         super().__init__()
         self.name = "contour"
         self.out_channels = 1
+        self.deduplicate = deduplicate
 
     def __call__(self, inst: np.ndarray, **kwargs) -> np.ndarray:
         """Generate contour transforms.
@@ -198,15 +251,24 @@ class ContourTransform(OnlyInstMapTransform):
             np.ndarray:
                 Contour of objects. Shape: (H, W). Dtype: float64
         """
-        return gen_contour_maps(fix_duplicates(inst))
+        if self.deduplicate:
+            inst = fix_duplicates(inst)
+        return gen_contour_maps(inst)
 
 
 class EdgeWeightTransform(OnlyInstMapTransform):
-    def __init__(self):
-        """Generate weight maps for object boundaries."""
+    def __init__(self, deduplicate: bool = True) -> None:
+        """Generate weight maps for object boundaries.
+
+        Parameters:
+            deduplicate (bool, default=True):
+                Whether to fix duplicate values in the mask before transforming.
+                This adds overhead, so use only if you know that the mask has duplicates.
+        """
         super().__init__()
         self.name = "edgeweight"
         self.out_channels = 1
+        self.deduplicate = deduplicate
 
     def __call__(self, inst: np.ndarray, **kwargs) -> np.ndarray:
         """Generate edge weight transforms.
@@ -219,6 +281,8 @@ class EdgeWeightTransform(OnlyInstMapTransform):
             np.ndarray:
                 Contour of objects. Shape: (H, W). Dtype: float64
         """
+        if self.deduplicate:
+            inst = fix_duplicates(inst)
         return gen_weight_maps(inst)
 
 
