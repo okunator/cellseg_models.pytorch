f7089b9675c432ffd8abc4b634ecbc2ed96bce43
Oskari <oskari.lehtonen@helsinki.fi>
2025-02-15 21:40:58 +0200

    rm: rm the root __init__ imports

--- cellseg_models_pytorch/__init__.py
+++ cellseg_models_pytorch/__init__.py
@@ -1,14 +1,2 @@
-from . import inference, models, utils
-from .models import CellPoseUnet, HoverNet, StarDistUnet
-
 __version__ = "0.1.25"
-submodules = ["utils", "models", "inference"]
-__all__ = [
-    "__version__",
-    "CellPoseUnet",
-    "StarDistUnet",
-    "HoverNet",
-    "utils",
-    "models",
-    "inference",
-]
+__all__ = ["__version__"]
